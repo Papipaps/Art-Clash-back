4baf9bce1e702ecbf6893d74427872b8b94a6dcd
Jordan Peltro <jordanpeltro@gmail.com>
2022-10-21 20:42:33 +0200

    init commit

---
+++ src/main/java/com/jojo/Artclash/ArtclashApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.Artclash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtclashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtclashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojo/Artclash/ArtclashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.Artclash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtclashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
